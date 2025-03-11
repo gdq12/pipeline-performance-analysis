e89a10031d7bc2a4edcd8b8c5803240bd8b272a1
gdq12 <gdquiceno@gmail.com>
2025-03-11 20:12:31 +0100

    updated exploratory queries based on new tbl structures

--- 1_extract_load_dataproc/extract_load_exploratory_queries.sql
+++ 1_extract_load_dataproc/extract_load_exploratory_queries.sql
@@ -1,72 +1,60 @@
--- which jobs failed to populate stage 
-select 
-  regexp_substr(data_source, '[a-z]{1,6}_tripdata_[0-9]{4}-[0-9]{2}') label
-  , query
-from pipeline-analysis-452722.nytaxi_monitoring.query_history_extract_load_spark
-where regexp_substr(error_result.message, 'Inserted row has wrong column count') is not null
-order by 1
-;
-
---what kind of error messages were there 
-select 
-  distinct regexp_substr(data_source, '[a-z]{1,6}_tripdata_[0-9]{4}-[0-9]{2}') label
-  , error_result.reason, error_result.message
-from pipeline-analysis-452722.nytaxi_monitoring.query_history_extract_load_spark
-where error_result.message is not null
-order by 1
-;
-
--- determine possible column name mismatch 
-select 
-  regexp_substr(data_source, '[a-z]{1,6}_tripdata_[0-9]{4}-[0-9]{2}') label
-  , regexp_substr(query, "on t1.column_name = '[a-z_].+'") 
-  , query , error_result.reason, error_result.message
-from pipeline-analysis-452722.nytaxi_monitoring.query_history_extract_load_spark
+-- are there any failed jobs
+SELECT  *
+FROM `pipeline-analysis-452722.nytaxi_monitoring.query_history_extract_load_spark`
 where error_result.message is not null
 ;
 
--- what jobs didnt make it to stage 
-select distinct data_source 
-from pipeline-analysis-452722.nytaxi_monitoring.query_history_extract_load_spark
-where data_source not in 
-(select data_source from pipeline-analysis-452722.nytaxi_stage.yellow_tripdata
-union distinct 
-select data_source from pipeline-analysis-452722.nytaxi_stage.yellow_tripdata_2009
-union distinct 
-select data_source from pipeline-analysis-452722.nytaxi_stage.yellow_tripdata_2010
-)
-order by 1
+-- get some initial stats on each query 
+SELECT 
+  regexp_substr(data_source, '[a-z]{1,6}_tripdata_[0-9]{4}-[0-9]{2}') label
+  , data_source
+  , start_time, end_time
+  , dml_statistics.inserted_row_count
+  , total_bytes_processed/pow(10,9) tota_gb_processed
+  , total_bytes_billed/pow(10,9) total_gb_billed
+  , job_id, query
+FROM pipeline-analysis-452722.nytaxi_monitoring.query_history_extract_load_spark
+where statement_type in ('INSERT', 'CREATE_TABLE_AS_SELECT')
+and regexp_substr(query, 'insert into `pipeline-analysis-452722.nytaxi_monitoring.query_history_extract_load_spark`') is null
 ;
 
--- check to see each parquet was loaded to stage
-with ext as
-(SELECT  
-  regexp_substr(data_source, '[a-z]{1,6}_tripdata_[0-9]{4}-[0-9]{2}') label
-  , data_source, table_name, num_row
-  , max(creation_dt) creation_dt, count(1) num_columns
-FROM `pipeline-analysis-452722.nytaxi_raw.extract_load_log`
-where regexp_substr(table_name, 'external') is not null 
-group by regexp_substr(data_source, '[a-z]{1,6}_tripdata_[0-9]{4}-[0-9]{2}')
-          , data_source, table_name, num_row
-), 
-stg as 
-(SELECT  
-  regexp_substr(data_source, '[a-z]{1,6}_tripdata_[0-9]{4}-[0-9]{2}') label
-  , data_source, table_name, num_row
-  , max(creation_dt) creation_dt, count(1) num_columns
-FROM `pipeline-analysis-452722.nytaxi_raw.extract_load_log`
-where regexp_substr(table_name, 'external') is null 
-group by regexp_substr(data_source, '[a-z]{1,6}_tripdata_[0-9]{4}-[0-9]{2}')
-          , data_source, table_name, num_row
-)
+-- get some current (size) stats for tables in stage 
 select 
-  ext.label, ext.data_source
-  , ext.table_name, stg.table_name
-  , ext.creation_dt, stg.creation_dt
-  , ext.num_row, stg.num_row
-  , ext.num_columns, stg.num_columns
-from ext ext 
-join stg stg on ext.label = stg.label 
-            and ext.data_source = stg.data_source 
-where ext.num_columns != stg.num_columns
+  project_id, dataset_id, table_id
+  , row_count
+  , size_bytes/pow(10,9) size_gb
+from pipeline-analysis-452722.nytaxi_stage.__TABLES__
+order by size_bytes desc
+;
+
+-- verify that all tables in stage are clustered
+select * 
+from pipeline-analysis-452722.nytaxi_stage.INFORMATION_SCHEMA.TABLES
+where regexp_substr(ddl, 'CLUSTER BY data_source') is not null
+-- just to make sure there is no clustering of another col 
+-- where regexp_substr(ddl, 'CLUSTER BY vendor_id') is not null 
+;
+
+-- try to determine the variation in col_name/data_types
+select 
+  distinct column_name, data_type, ordinal_position
+from pipeline-analysis-452722.nytaxi_stage.INFORMATION_SCHEMA.COLUMNS
+order by column_name
+;
+
+-- spot check to see if there are records that dont pertain to the respective source parquet
+-- quite difficult to do with all the different data types, better to do post the col cleaning
+select * from 
+(select 
+  parse_datetime('%Y-%m-%d', regexp_substr(data_source, '[0-9]{4}-[0-9]{2}$')||'-01') start_date
+  , last_day(parse_date('%Y-%m-%d', regexp_substr(data_source, '[0-9]{4}-[0-9]{2}$')||'-01'), month) end_date
+  , *
+from `pipeline-analysis-452722.nytaxi_stage.yellow_tripdata_2009-01`
+)
+
+where (
+  -- (start_date > parse_datetime('%Y-%m-%d %H:%M:%S', trip_pickup_date_time))
+  -- or
+  (end_date < parse_datetime('%Y-%m-%d %H:%M:%S', trip_dropoff_date_time))
+) 
 ;
