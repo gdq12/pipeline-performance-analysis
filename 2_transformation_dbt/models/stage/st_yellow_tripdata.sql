808fb406b07b102a3fd4cbca0f81fbfc726bb529
gdq12 <gdquiceno@gmail.com>
2025-03-19 08:01:18 +0100

    queries to collate ALL yellow data into a single table complete

---
+++ 2_transformation_dbt/models/stage/st_yellow_tripdata.sql
@@ -0,0 +1,93 @@
+{{ config(materialized='table') }}
+
+with t1 as
+(select 
+    {{ update_vendor_id("vendor_id") }} vendor_id
+    ,cast(pickup_datetime as timestamp) pickup_datetime 
+    ,cast(dropoff_datetime as timestamp) dropoff_datetime
+    ,{{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} passenger_count
+    ,cast(trip_distance as float64) trip_distance
+    ,{{ dbt.safe_cast("pickup_location_id", api.Column.translate_type("integer")) }} pickup_location_id
+    ,{{ dbt.safe_cast("ratecode_id", api.Column.translate_type("string")) }} ratecode_id
+    ,{{ update_store_and_fwd_flag("store_and_fwd_flag") }}  store_and_fwd_flag
+    ,{{ dbt.safe_cast("dropoff_location_id", api.Column.translate_type("integer")) }} dropoff_location_id
+    ,{{ update_payment_type("update_payment_type") }} payment_type
+    ,cast(fare_amount as float64) fare_amount
+    ,cast(mta_tax as float64) mta_tax
+    ,cast(tip_amount as float64) tip_amount
+    ,cast(tolls_amount as float64) tolls_amount
+    ,cast(null as float64) improvement_surcharge
+    ,cast(total_amount as float64) total_amount
+    ,cast(congestion_surcharge as float64) congestion_surcharge
+    ,cast(pickup_date as timestamp) pickup_date
+    ,{{ dbt.safe_cast("trip_type", api.Column.translate_type("string")) }} trip_type
+    ,cast(data_start_date as timestamp) data_start_date
+    ,cast(data_end_date as timestamp) data_end_date
+    ,{{ dbt.safe_cast("data_source", api.Column.translate_type("string")) }} data_source
+    ,cast(creation_dt as timestamp) creation_dt
+from {{ ref('vw_yellow_tripdata_2009_2010') }}
+where pickup_datetime < dropoff_datetime
+union all 
+select 
+    {{ update_vendor_id("vendor_id") }} vendor_id
+    ,cast(pickup_datetime as timestamp) pickup_datetime 
+    ,cast(dropoff_datetime as timestamp) dropoff_datetime
+    ,{{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} passenger_count
+    ,cast(trip_distance as float64) trip_distance
+    ,{{ dbt.safe_cast("pickup_location_id", api.Column.translate_type("integer")) }} pickup_location_id
+    ,{{ dbt.safe_cast("ratecode_id", api.Column.translate_type("string")) }} ratecode_id
+    ,{{ update_store_and_fwd_flag("store_and_fwd_flag") }}  store_and_fwd_flag
+    ,{{ dbt.safe_cast("dropoff_location_id", api.Column.translate_type("integer")) }} dropoff_location_id
+    ,{{ update_payment_type("update_payment_type") }} payment_type
+    ,cast(fare_amount as float64) fare_amount
+    ,cast(mta_tax as float64) mta_tax
+    ,cast(tip_amount as float64) tip_amount
+    ,cast(tolls_amount as float64) tolls_amount
+    ,cast(improvement_surcharge as float64) improvement_surcharge
+    ,cast(total_amount as float64) total_amount
+    ,cast(congestion_surcharge as float64) congestion_surcharge
+    ,cast(pickup_date as timestamp) pickup_date
+    ,{{ dbt.safe_cast("trip_type", api.Column.translate_type("string")) }} trip_type
+    ,cast(data_start_date as timestamp) data_start_date
+    ,cast(data_end_date as timestamp) data_end_date
+    ,{{ dbt.safe_cast("data_source", api.Column.translate_type("string")) }} data_source
+    ,cast(creation_dt as timestamp) creation_dt
+from {{ ref('vw_yellow_tripdata') }}
+where pickup_datetime < dropoff_datetime
+)
+select 
+    {{ dbt_utils.generate_surrogate_key( ['vendor_id', 'pickup_datetime ', 'dropoff_datetime', 'passenger_count', 
+                                        'trip_distance', 'pickup_location_id', 'ratecode_id', 'store_and_fwd_flag', 
+                                        'dropoff_location_id', 'payment_type', 'fare_amount', 'mta_tax', 'tip_amount', 
+                                        'tolls_amount', 'improvement_surcharge', 'total_amount', 'congestion_surcharge',
+                                        'data_source']) }} trip_id
+    ,vendor_id
+    ,pickup_datetime 
+    ,dropoff_datetime
+    ,passenger_count
+    ,trip_distance
+    ,pickup_location_id
+    ,ratecode_id
+    ,store_and_fwd_flag
+    ,dropoff_location_id
+    ,payment_type
+    ,fare_amount
+    ,mta_tax
+    ,tip_amount
+    ,tolls_amount
+    ,improvement_surcharge
+    ,total_amount
+    ,congestion_surcharge
+    ,pickup_date
+    ,trip_type
+    ,data_start_date
+    ,data_end_date
+    ,data_source
+    ,creation_dt
+from t1 
+
+{% if var('is_test_run', default = true) %}
+
+  limit 100 
+
+{% endif %}
