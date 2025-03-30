abf013e775c6d371355ee3788b07cc5f8dc0ed39
gdq12 <gdquiceno@gmail.com>
2025-03-30 17:44:21 +0200

    loaded mapping tables to bigquery

--- 1_extract_load_dataproc/extract-load-mapping-tables.sql
+++ 1_extract_load_dataproc/extract-load-mapping-tables.sql
@@ -4,10 +4,10 @@ options (location = 'EU')
 ;
 
 -- bring data from bucket to then push to mapping schema 
-create or replace external table `${PROJECT_ID}`.`nytaxi_raw`.`mapping_data`
+create or replace external table `${PROJECT_ID}`.`nytaxi_raw_backup`.`mapping_data`
 options (
 format = 'CSV',
-uris = ['gs://helper-data/bigquery_public_data_taxi_zone_geom.csv']
+uris = ['gs://helper-data2/bigquery_public_data_taxi_zone_geom.csv']
 )
 ;
 
@@ -18,13 +18,13 @@ select
   , borough
   , ST_GEOGFROMTEXT(zone_geom) zone_geom
   , current_timestamp()
-from `${PROJECT_ID}`.`nytaxi_raw`.`mapping_data`
+from `${PROJECT_ID}`.`nytaxi_raw_backup`.`mapping_data`
 ;
 
-create or replace external table `${PROJECT_ID}`.`nytaxi_raw`.`mapping_data`
+create or replace external table `${PROJECT_ID}`.`nytaxi_raw_backup`.`mapping_data`
 options (
 format = 'CSV',
-uris = ['gs://helper-data/hvlv_base_numbers.csv'],
+uris = ['gs://helper-data2/hvlv_base_numbers.csv'],
 skip_leading_rows = 1
 )
 ;
@@ -32,18 +32,18 @@ skip_leading_rows = 1
 create table if not exists `${PROJECT_ID}`.`nytaxi_mapping`.`hvlv_base_numbers`
 (hvln string, license_base_number string, base_name string, app_company_affiliation string, creation_dt timestamp) as
 select *, current_timestamp()
-from `${PROJECT_ID}`.`nytaxi_raw`.`mapping_data`
+from `${PROJECT_ID}`.`nytaxi_raw_backup`.`mapping_data`
 ;
 
-create or replace external table `${PROJECT_ID}`.`nytaxi_raw`.`mapping_data`
+create or replace external table `${PROJECT_ID}`.`nytaxi_raw_backup`.`mapping_data`
 options (
 format = 'CSV',
-uris = ['gs://helper-data/taxi_zone_lookup.csv']
+uris = ['gs://helper-data2/taxi_zone_lookup.csv']
 )
 ;
 
 create table if not exists `${PROJECT_ID}`.`nytaxi_mapping`.`taxi_zone_lookup`
 (location_id integer, borough string, zone string, service_zone string, creation_dt timestamp) as
 select *, current_timestamp()
-from `${PROJECT_ID}`.`nytaxi_raw`.`mapping_data`
+from `${PROJECT_ID}`.`nytaxi_raw_backup`.`mapping_data`
 ;
